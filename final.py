91736e4089a853c19524d5951ed0d6a1dc9b35aa
Rovin Raj <68719290+rovinraj@users.noreply.github.com>
2023-10-22 12:35:02 -0500

    Update final.py
    remove button + barcode error handling

--- final.py
+++ final.py
@@ -14,10 +14,22 @@ def BarcodeReader(image):
     if detectedBarcodes:
         for barcode in detectedBarcodes:
             if barcode.data != "": 
-                #return ["Oojami - Time Is Now (Music CD)", "https://images.barcodelookup.com/449/4498117-1.jpg"]
-                response = requests.get("https://api.barcodelookup.com/v3/products?barcode=" + barcode.data.decode(
-                    "utf-8") + "&formatted=y&key=k2hu41evicezr4pqgu9tui9jth1q4t")
-                return [response.json()['products'][0]['title'], response.json()['products'][0]['images'][0]]
+                try:
+                    #return ["Oojami - Time Is Now (Music CD)", "https://images.barcodelookup.com/449/4498117-1.jpg"]
+                    response = requests.get("https://api.barcodelookup.com/v3/products?barcode=" + barcode.data.decode(
+                        "utf-8") + "&formatted=y&key=qim1lskytd7i8gj8606javkwjpnw50")
+                    response.raise_for_status()
+                    data = response.json()
+                    return [data['products'][0]['title'], data['products'][0]['images'][0]]
+                except requests.exceptions.HTTPError as http_err:
+                    print(f"HTTP error occurred: {http_err}")
+                    st.write(f"HTTP error occurred: {http_err}")
+                except requests.exceptions.RequestException as req_err:
+                    print(f"Request exception occurred: {req_err}")
+                    st.write(f"Request exception occurred: {req_err}")
+                except json.decoder.JSONDecodeError as json_err:
+                    print("Error parsing JSON data:", json_err)
+                    st.write("Error parsing JSON data:", json_err)
 
 
 def getImage(cam, curtime, boxes, placeholder):
@@ -71,25 +83,44 @@ def barcode(curtime):
 def inventory():
     st.title("Inventory")
     st.markdown('<hr style="border: 0; height: 1px; background-color: #ccc;">', unsafe_allow_html=True)
-    arr = st.session_state.items
-    print(arr)
+    
+    # Read the inventory from the file
     with open("inven.txt", "r") as file:
-        for line in file:
-            i = line.strip().split(",", 1)
-            st.markdown(
-    f"""
-    <div style="display: flex; align-items: center; justify-content: space-between;">
-        <div style="font-family: 'Lexend Deca', 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 24px; line-height: 1.5;">
-            {i[0]}
-        </div>
-        <div style="float: right;">
-            <img src="{i[1]}" alt="Image" style="width: 100px; max-width: 100%; height: auto;">
-        </div>
-    </div>
-    <hr style="border: 0; height: 1px; background-color: #ccc;">
-    """,
-    unsafe_allow_html=True
-)
+        inventory_lines = file.readlines()
+    
+    # Create a list to store buttons and their corresponding lines
+    buttons_and_lines = []
+    
+    # Display each item in the inventory along with a "Remove" button
+    for i, line in enumerate(inventory_lines):
+        item, img_url = line.strip().rsplit(",", 1)
+        st.markdown(
+            f"""
+            <div style="display: flex; align-items: center; justify-content: space-between;">
+                <div style="font-family: 'Lexend Deca', 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 24px; line-height: 1.5;">
+                    {item}
+                </div>
+                <div style="float: right;">
+                    <img src="{img_url}" alt="Image" style="width: 100px; max-width: 100%; height: auto;">
+                </div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        
+        # Create a "Remove" button and associate it with the line
+        if st.button("Remove Item", key=i):
+            buttons_and_lines.append((i, line))
+        
+        st.markdown('<hr style="border: 0; height: 1px; background-color: #ccc;">', unsafe_allow_html=True)
+
+    # Remove items associated with the clicked buttons
+    for index, line in buttons_and_lines:
+        inventory_lines.pop(index)
+
+    # Update the inventory file with the modified content
+    with open("inven.txt", 'w') as filedata:
+        filedata.writelines(inventory_lines)
 
 
 def suggestion():
